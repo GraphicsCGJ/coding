828c465a7f855a15c215e92d95e27fc564f242aa
gyujin_choi <gyujin_choi@tmax.co.kr>
2021-12-03 14:56:52 +0900

    .
    .

---
+++ cpp/acm/1436.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include<set>
+
+using namespace std;
+
+
+const int tmp = 666;
+
+int main(void) {
+    set<int, std::less<int>> s;
+
+    /* insert */
+    for (int i = 0; i < 5; i++) {
+        int sum = 0;
+        sum += (int)pow(10, i) * tmp;
+        for ()
+    }
+
+    return 0;
+}
