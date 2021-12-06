04ec903ba9381b654d7f8e437d88297dd880d60b
gyujin_choi <gyujin_choi@tmax.co.kr>
2021-12-06 14:55:37 +0900

    # git log format # made by gunmin_lee 20200129
    분류: , 제목: 
    분류 - 기능추가, 오류해결, 성능개선
    
    변경 목적 - 왜 이 commit 작업을 하게 되었습니까?
    
    변경 내용 작업한 코드 및 모듈, 작업내용 작성
    
    테스트 시나리오 - 연구소 및 QA테스트를 위한 시나리오를 작성해주세요
    
    매뉴얼 반영 위치 - 매뉴얼에 반영 할 내용이라면 반영된 매뉴얼 위치를 적어주세요
    
    IMS번호 - IMS에 등록된 issue해결을 위한 commit일 경우 IMS번호를 입력하세요
    
    [ 리뷰어 : ] 가급적 commit(push)전 팀원들과 코드리뷰를 진행해주세요

--- cpp/acm/1759.cpp
+++ cpp/acm/1759.cpp
@@ -4,13 +4,68 @@
 
 using namespace std;
 
+vector<char> vowelsVector;
+
+bool check(vector<char> v) {
+
+    int consonants = 0;
+    int vowels = 0;
+    for (const auto& i : v) {
+        if (find(vowelsVector.begin(), vowelsVector.end(), i) != vowelsVector.end())
+            vowels++;
+        else
+            consonants++;
+    }
+
+    // cout << consonants << " , " << vowels << endl;
+
+    if (consonants >= 2 && vowels >= 1)
+        return true;
+    return false;
+}
+
 bool cmp (char a, char b) {
     return a < b;
+}
+
+void backtracking(vector<char> &v, vector<char> &v2, int i, int n) {
+    // for (const char& c : v2) {
+    //     cout << c;
+    // }
+    // cout << endl;
+
+    if (v.size() - i < n - v2.size())
+        return;
+    if (v2.size() == n) {
+        if (check(v2)) {
+            for (const char& c : v2) {
+                cout << c;
+            }
+            cout << endl;
+        }
+        return;
+    }
+    if (i == v.size())
+        return;
+
+
+    if (v2.size() < n) { // can insert
+        v2.push_back(v[i]);
+        backtracking(v, v2, i+1, n);
+        v2.pop_back();
+        backtracking(v, v2, i+1, n);
+    }
 }
 
 int main(void) {
     int l, c;
     cin >> l >> c;
+
+    vowelsVector.push_back('a');
+    vowelsVector.push_back('e');
+    vowelsVector.push_back('i');
+    vowelsVector.push_back('o');
+    vowelsVector.push_back('u');
 
     vector<char> v;
     for (int i = 0; i < c; i++) {
@@ -21,10 +76,12 @@ int main(void) {
 
     sort(v.begin(), v.end(), cmp);
 
-    // for (const auto& i : v) {
-    //     cout << i << endl;
-    // }
+    for (int i = 0; i <= v.size() - l; i++) {
+        vector<char> v_tmp;
+        v_tmp.push_back(v[i]);
 
+        backtracking(v, v_tmp, i+1, l);
+    }
 
     return 0;
 }
