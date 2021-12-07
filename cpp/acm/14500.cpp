42a9815250115af438cc52624d23510627caf60b
gyujin_choi <gyujin_choi@tmax.co.kr>
2021-12-07 16:38:28 +0900

    .
    .

--- cpp/acm/14500.cpp
+++ cpp/acm/14500.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <queue>
+#include <stack>
 
 using namespace std;
 
 int dx[4] = {0, 0, 1, -1};
 int dy[4] = {1, -1, 0, 0};
+
+int maxVal;
 
 bool check_tet( // aa is not in tet
     vector<pair<int, int>>& tet,
@@ -17,7 +19,7 @@ bool check_tet( // aa is not in tet
             (visited[aa.first][aa.second] == 0 ? true : false);
 }
 
-int dfs(
+void backtracking(
     vector<vector<int>>& v,
     vector<vector<int>>& visited,
     int n,
@@ -25,28 +27,38 @@ int dfs(
     int si,
     int sj
 ) {
-    queue<pair<int, int>> q;
+    stack<pair<int, int>> s;
     vector<pair<int, int>> tet;
-    q.push(pair<int, int>(si, sj));
+    s.push(pair<int, int>(si, sj));
 
-    while (!q.empty()) {
-        int li = q.front().first;
-        int lj = q.front().second;
+    while (!s.empty()) {
+        int li = s.top().first;
+        int lj = s.top().second;
 
-        q.pop();
+        s.pop();
+
+        if (li == -1 && lj == -1) {
+            tet.pop_back();
+            continue;
+        }
+
         tet.push_back(pair<int, int>(li, lj));
 
         if (tet.size() == 4) {
             /* calc */
+            int sum = 0;
             for (int i = 0; i < 4; i++) {
-                cout << i << " ";
+                sum += v[tet[i].first][tet[i].second];
+                // cout << tet[i].first << ":" << tet[i].second << " ";
             }
-            cout << endl;
+            if (sum > maxVal)
+                maxVal = sum;
 
             tet.pop_back();
-            continue;
         }
         else {
+            pair<int, int> tet_check(-1, -1);
+            s.push(tet_check);
             for (int i = 0; i < 4; i++) {
                 int li2 = li + dx[i];
                 int lj2 = lj + dy[i];
@@ -57,12 +69,10 @@ int dfs(
                 if (li2 < 0 || li2 >= n || lj2 < 0 || lj2 >= m || !check_tet(tet, visited, lp))
                     continue;
 
-                q.push(lp);
+                s.push(lp);
             }
         }
     }
-
-    return 0;
 }
 
 int main(void) {
@@ -76,24 +86,22 @@ int main(void) {
         }
     }
 
-    int max = 0;
+    maxVal = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             vector<vector<int>> visited(n, vector<int>(m, 0));
 
             for (int ii = 0; ii <= i; ii++) {
-                int jj = (ii == i) ? j + 1 : m;
+                int jj = (ii == i) ? j : m;
                 fill(&visited[ii][0], &visited[ii][jj], 1);
             }
 
-            /* bfs */
-            int local_max = dfs(v, visited, n, m, i, j);
-            if (local_max > max)
-                max = local_max;
+            /* back tracking */
+            backtracking(v, visited, n, m, i, j);
         }
     }
 
-    cout << max << endl;
+    cout << maxVal << endl;
 
     return 0;
 }
