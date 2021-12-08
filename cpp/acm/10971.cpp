90cf9dffab19e5965556ec93a331b10205fcb062
gyujin_choi <ckjin95@gmail.com>
2021-12-08 14:18:50 +0900

    good!

---
+++ cpp/acm/10971.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+#define MAX_VAL 10
+
+int w[MAX_VAL][MAX_VAL];
+
+int main(void) {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i ++) {
+        for (int j = 0; j < n; j ++) {
+
+        }
+    }
+}
--- cpp/acm/14500.cpp
+++ cpp/acm/14500.cpp
@@ -1,50 +1,28 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 #include <stack>
-#include <set>
+#include <cstring>
 
 using namespace std;
+#define SIZE 500
 
-
-class tmp_c {
-    public:
-        vector<pair<int, int>> locate;
-        int value;
-        bool operator() (const tmp_c& a, const tmp_c& b) const {
-        return ((a.locate[0] < b.locate[0]) ||
-            (!(a.locate[0] > b.locate[0]) && a.locate[1] < b.locate[1]) ||
-            (!(a.locate[0] > b.locate[0]) && !(a.locate[1] > b.locate[1]) && a.locate[2] < b.locate[2]) ||
-            (!(a.locate[0] > b.locate[0]) && !(a.locate[1] > b.locate[1]) && !(a.locate[2] > b.locate[2]) && a.locate[3] < b.locate[3]));
-        }
-};
-
-set<tmp_c, tmp_c> memoiz2;
+int n, m;
 
 int dx[4] = {0, 0, 1, -1};
 int dy[4] = {1, -1, 0, 0};
 
+int v[SIZE][SIZE];
+bool visited[SIZE][SIZE];
+
 int maxVal;
+vector<pair<int, int>> tet;
 
-bool check_tet( // aa is not in tet
-    vector<pair<int, int>>& tet,
-    vector<vector<int>>& visited,
-    pair<int, int> aa
-) {
-    return (find(tet.begin(), tet.end(), aa) == tet.end() ? true : false) &&
-            (visited[aa.first][aa.second] == 0 ? true : false);
-}
-
-void backtracking(
-    vector<vector<int>>& v,
-    vector<vector<int>>& visited,
-    int n,
-    int m,
+void dfs(
     int si,
     int sj
 ) {
+
     stack<pair<int, int>> s;
-    vector<pair<int, int>> tet;
     s.push(pair<int, int>(si, sj));
 
     while (!s.empty()) {
@@ -52,32 +30,28 @@ void backtracking(
         int lj = s.top().second;
 
         s.pop();
-
         if (li == -1 && lj == -1) {
+
+            visited[tet.back().first][tet.back().second] = false;
             tet.pop_back();
+
             continue;
         }
 
         tet.push_back(pair<int, int>(li, lj));
+        visited[li][lj] = true;
 
         if (tet.size() == 4) {
             /* calc */
-
-            tmp_c tc;
-            tc.locate = tet;
-            tc.value = 0;
-            auto it = memoiz2.find(tc);
-
-            if (it == memoiz2.end()) {
-                for (int i = 0; i < 4; i++) {
-                    tc.value += v[tc.locate[i].first][tc.locate[i].second];
-                }
-                memoiz2.insert(tc);
-
-                if (tc.value > maxVal)
-                    maxVal = tc.value;
+            int sum = 0;
+            for (int i = 0; i < 4; i++) {
+                sum += v[tet[i].first][tet[i].second];
             }
 
+            if (sum > maxVal)
+                maxVal = sum;
+
+            visited[tet.back().first][tet.back().second] = false;
             tet.pop_back();
         }
         else {
@@ -85,30 +59,37 @@ void backtracking(
             s.push(tet_check);
 
             for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < tet.size(); j++) {
-                    int li = tet[j].first;
-                    int lj = tet[j].second;
-                    int li2 = li + dx[i];
-                    int lj2 = lj + dy[i];
-                    pair<int, int> lp;
-                    lp.first = li2;
-                    lp.second = lj2;
+                pair<int, int> lp;
+                lp.first = li + dx[i];
+                lp.second = lj + dy[i];;
 
-                    if (li2 < 0 || li2 >= n || lj2 < 0 || lj2 >= m || !check_tet(tet, visited, lp))
-                        continue;
+                if (lp.first < 0 || lp.first >= n || lp.second < 0 || lp.second >= m || visited[lp.first][lp.second] == true)
+                    continue;
 
-                    s.push(lp);
-                }
+                s.push(lp);
             }
         }
     }
 }
 
-int main(void) {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int>> v(n, vector<int>(m, 0));
+void case1(int i, int j) { // ㅓ ㅏ
+    int sum = v[i+1][j] + v[i][j+1] + v[i+1][j+1] + v[i+2][j+1];
+    if (sum > maxVal) maxVal = sum;
+    sum = v[i+1][j+1] + v[i][j] + v[i+1][j] + v[i+2][j];
+    if (sum > maxVal) maxVal = sum;
+}
 
+void case2(int i, int j) { // ㅗ ㅜ
+    int sum = v[i][j+1] + v[i+1][j] + v[i+1][j+1] + v[i+1][j+2];
+    if (sum > maxVal) maxVal = sum;
+    sum = v[i+1][j+1] + v[i][j] + v[i][j+1] + v[i][j+2];
+    if (sum > maxVal) maxVal = sum;
+}
+
+int main(void) {
+    tet.reserve(100);
+
+    cin >> n >> m;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cin >> v[i][j];
@@ -118,25 +99,15 @@ int main(void) {
     maxVal = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            vector<vector<int>> visited(n, vector<int>(m, 0));
-
-            for (int ii = 0; ii <= i; ii++) {
-                int jj = (ii == i) ? j : m;
-                fill(&visited[ii][0], &visited[ii][jj], 1);
-            }
+            memset(visited, false, sizeof(visited));
 
             /* back tracking */
-            backtracking(v, visited, n, m, i, j);
+            dfs(i, j);
+            /* exception cases */
+            if (i + 2 < n && j + 1 < m) case1(i, j);
+            if (i + 1 < n && j + 2 < m) case2(i, j);
         }
     }
-
-    // for (auto& i : memoiz2) {
-    //     for (int j = 0; j < 4; j++) {
-    //         cout << "[" << i.locate[j].first << "," << i.locate[j].second << "]";
-    //     }
-    //     cout << endl;
-    // }
-
     cout << maxVal << endl;
 
     return 0;
