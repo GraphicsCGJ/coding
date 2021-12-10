4d3e1afbd47edaaf0fd45fb12b3ca863fa064837
gyujin_choi <ckjin95@gmail.com>
2021-12-11 02:16:55 +0900

    good~

---
+++ cpp/acm/1038.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+int used;
+
+void get_val(int *cur_val, int& used) {
+
+}
+
+int main(void) {
+    int used_bit = 1;
+    vector <long long> v;
+    int final_val[10] {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    int cur_val[10] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    while (cur_val != final_num) {
+
+        v.push_back()
+    }
+
+
+    return 0;
+}
