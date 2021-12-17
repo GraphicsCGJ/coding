64fcd84953353d57fdd2b01a109107cb16f3c4f7
gyujin_choi <gyujin_choi@tmax.co.kr>
2021-12-17 17:54:06 +0900

    good~

--- cpp/acm/1038.cpp
+++ cpp/acm/1038.cpp
@@ -11,11 +11,24 @@ void get_val(int *cur_val, int& used) {
 
 int main(void) {
     int used_bit = 1;
-    vector <long long> v;
-    int final_val[10] {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    int final_val[10] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int cur_val[10] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-    while (cur_val != final_num) {
+    while (true) {
+        bool flag = true;
+        for (int i = 0; i < 10; i++) {
+            if (final_val[i] != cur_val[i]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag == true)
+            break;
+
+        /* 1. check cur_val is descending number. */
+        for (int i = 1; i < used_bit; i++) {
+
+        }
 
         v.push_back()
     }
