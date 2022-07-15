dbe5d4b5380a8cd743623f4054c74d7476145a80
gyujin_choi <gyujin_choi@tmax.co.kr>
2022-07-15 19:28:06 +0900

    ci
    ci

--- cpp/acm/1011.cpp
+++ cpp/acm/1011.cpp
@@ -2,45 +2,7 @@
 
 using namespace std;
 
-class problem {
-    public:
-        int x;
-        int y;
-        int knx;
-        int kny;
-        int kx;
-        int ky;
-        int cnt;
-
-        problem() {
-            x = 0;
-            y = 0;
-            kx = 0;
-            ky = 0;
-            knx = 0;
-            kny = 0;
-            cnt = 0;
-        }
-
-        void input() {
-            cin >> x;
-            cin >> y;
-
-            kx = x - x;
-            ky = y - x;
-
-            knx = 0;
-            kny = 0;
-        }
-
-        void calc() {
-
-        }
-};
-
 int main(void) {
-    /*
-        difference 를 구하고
-    */
+
     return 0;
 }
---
+++ cpp/acm/1024.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int L;
+int main(void) {
+    cin >> N >> L;
+
+    for (int i = L; i <= 100; i++) {
+
+    }
+
+
+    return 0;
+}
