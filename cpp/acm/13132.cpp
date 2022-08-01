e4f0b951ddbde9999d604b05b599bef280f9ea77
gyujin_choi <gyujin_choi@tmax.co.kr>
2022-08-01 14:35:18 +0900

    # git log format # made by gunmin_lee 20200129
    분류: , 제목: 
    분류 - 기능추가, 오류해결, 성능개선
    
    변경 목적 - 왜 이 commit 작업을 하게 되었습니까?
    
    변경 내용 작업한 코드 및 모듈, 작업내용 작성
    
    테스트 시나리오 - 연구소 및 QA테스트를 위한 시나리오를 작성해주세요
    
    매뉴얼 반영 위치 - 매뉴얼에 반영 할 내용이라면 반영된 매뉴얼 위치를 적어주세요
    
    IMS번호 - IMS에 등록된 issue해결을 위한 commit일 경우 IMS번호를 입력하세요
    
    [ 리뷰어 : ] 가급적 commit(push)전 팀원들과 코드리뷰를 진행해주세요

---
+++ cpp/acm/13132.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+float M, B;
+
+class ca {
+public:
+    int p;
+    float rate;
+};
+
+for (int cdepth, int depth, int idx, float money, float possibility) {
+    if (money < B) {
+
+    }
+}
+
+int main(void) {
+    cin >> N >> M >> B;
+
+    for (int i = 0; i < N; i++) {
+        ca cai;
+        cin >> cai.p >> cai.rate;
+    }
+
+    return 0;
+}
