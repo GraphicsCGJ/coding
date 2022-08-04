76428edf9865412c58b1699c3cbe0bc675b65be0
gyujin_choi <gyujin_choi@tmax.co.kr>
2022-08-04 19:20:54 +0900

    push
    .

--- cpp/acm/1106.cpp
+++ cpp/acm/1106.cpp
@@ -10,17 +10,17 @@ typedef struct {
 
 knap arr[21];
 
-int dp[21][1001];
+int dp[21][2000];
 
 int main(void) {
     cin >> C >> N;
     for (int i = 1; i <= N; i++) {
         cin >> arr[i].cost >> arr[i].people;
-        cout << arr[i].cost << " " << arr[i].people << endl;
     }
 
+    int mincost = 50000000;
     for (int i = 0; i <= N; i++) {
-        for (int j = 0; j <= C; j++) {
+        for (int j = 0; j <= 2000; j++) {
             if (i == 0)
                 dp[i][j] = 0;
             else {
@@ -28,26 +28,31 @@ int main(void) {
                     int local_max = dp[i - 1][j];
                     for (int idx = 1; j >= arr[i].cost * idx; idx++) {
                         int local_max2 = dp[i - 1][j - arr[i].cost * idx] + arr[i].people * idx;
-                        // int local_max2 = 1;
                         if (local_max < local_max2) local_max = local_max2;
                     }
-                    // cout << i << " " << j << " " << local_max << endl;;
                     dp[i][j] = local_max;
                 }
                 else {
                     dp[i][j] = dp[i - 1][j];
                 }
+                if (dp[i][j] >= C) {
+                    if (mincost > j) {
+                        mincost = j;
+                    }
+                }
             }
         }
     }
 
-    // cout << dp[N][C] << endl;
-    for (int i = 0; i <= N; i ++) {
-        for (int j = 0; j <= C; j++) {
-            cout << dp[i][j] << "  ";
-        }
-        cout << endl;
-    }
+    // // cout << dp[N][C] << endl;
+    // for (int i = 0; i <= N; i ++) {
+    //     for (int j = 0; j <= C; j++) {
+    //         cout << j<<":"<<dp[i][j] << "  ";
+    //     }
+    //     cout << endl;
+    // }
+    cout << mincost << endl;
+
 
     return 0;
 }
