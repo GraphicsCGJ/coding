d3985e6cf40a6828d041dbe3c273982a3aff91d9
gyujin_choi <ckjin95@gmail.com>
2023-02-07 22:32:46 +0900

    21758

--- cpp/acm/workspace.cpp
+++ cpp/acm/workspace.cpp
@@ -3,9 +3,16 @@
 #include <queue>
 using namespace std;
 
+typedef long long ll;
+
 int N, K;
 int arr1[300000];
-int arr2[300000];
+ll sumarr[300000];
+
+inline ll rangesum(const int& l, const int& r) {
+    if (r < l) return 0;
+    return (sumarr[r] - sumarr[l] + arr1[l]);
+}
 
 int main(void) {
     cin.tie(0);
@@ -13,13 +20,27 @@ int main(void) {
 
     cin >> N;
 
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++) {
         cin >> arr1[i];
+        sumarr[i] = i == 0 ? 0 : sumarr[i-1] + arr1[i];
+    }
 
-    bool flag = arr1[0] < arr1[N-1];
-    int s = flag ? 1 : 0, e = flag ? N-2 : N-1;
+    ll MX = -1;
 
+    for (int i = 1; i < N-1; i++) {
+        ll l = rangesum(1, i-1);
+        ll r = rangesum(i+1, N-2);
 
+        ll localMX = l + r + r + arr1[N-1] * 2;
+        ll localMX2 = l + l + r + arr1[0] * 2;
+        ll localMX3 = rangesum(1, i) + rangesum(i, N-2);
+        localMX = localMX < localMX2 ? localMX2 : localMX;
+        localMX = localMX < localMX3 ? localMX3 : localMX;
+
+        MX = MX < localMX ? localMX : MX;
+    }
+
+    cout << MX << '\n';
 
     return 0;
 }
