53095f3342e4531046ac194916003f8fd94a3dbb
gyujin_choi <ckjin95@gmail.com>
2023-02-11 22:16:01 +0900

    hh

--- cpp/acm/workspace.cpp
+++ cpp/acm/workspace.cpp
@@ -1,46 +1,47 @@
-#include <iostream>
-#include <algorithm>
-#include <queue>
+#include<iostream>
+#include<string>
+
 using namespace std;
 
-typedef long long ll;
+string s;
+int lvl = 0;
+int p = 0;
 
-int N, K;
-int arr1[300000];
-ll sumarr[300000];
+class C1 {
+public:
+    C1() = default;
+    char op;
+    C1* l;
+    C1* r;
+};
 
-inline ll rangesum(const int& l, const int& r) {
-    if (r < l) return 0;
-    return (sumarr[r] - sumarr[l] + arr1[l]);
+C1 makeTree() {
+    int curlvl = lvl;
+    C1 c1;
+
+    while (p < s.length()) {
+        char c = s[p];
+
+        if (c == '(') {
+            p++;
+            C1 c2 = makeTree();
+        }
+        else if (c == ')') {
+            return c1;
+        }
+        else if (c ==   )
+
+        p++;
+    }
+
+    return c1;
 }
 
 int main(void) {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
 
-    cin >> N;
-
-    for (int i = 0; i < N; i++) {
-        cin >> arr1[i];
-        sumarr[i] = i == 0 ? 0 : sumarr[i-1] + arr1[i];
-    }
-
-    ll MX = -1;
-
-    for (int i = 1; i < N-1; i++) {
-        ll l = rangesum(1, i-1);
-        ll r = rangesum(i+1, N-2);
-
-        ll localMX = l + r + r + arr1[N-1] * 2;
-        ll localMX2 = l + l + r + arr1[0] * 2;
-        ll localMX3 = rangesum(1, i) + rangesum(i, N-2);
-        localMX = localMX < localMX2 ? localMX2 : localMX;
-        localMX = localMX < localMX3 ? localMX3 : localMX;
-
-        MX = MX < localMX ? localMX : MX;
-    }
-
-    cout << MX << '\n';
+    cin >> s;
 
     return 0;
 }
