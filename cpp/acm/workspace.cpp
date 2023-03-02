f1cfb700cbb7d69a52b218de0273a01610a3aa3a
gyujin_choi <gyujin_choi@tmax.co.kr>
2023-03-02 19:17:40 +0900

    git commit

--- cpp/acm/workspace.cpp
+++ cpp/acm/workspace.cpp
@@ -3,55 +3,77 @@
 #include <cmath>
 using namespace std;
 
-int N;
+int T;
 
-inline bool isSosu(int val) {
-  if (val < 2) return false;
-  else if (val == 2) return true;
-  else if (val % 2 == 0) return false;
+class vec {
+public:
+  int x;
+  int y;
 
-  for (int i = 3; i< (int)sqrt(val) + 1; i+=2)
-    if (val % i == 0) return false;
+  vec() = default;
+  vec& operator=(const vec& rhs) = default;
+  vec(int x, int y) {this->x = x; this->y = y;}
 
-  return true;
-}
+  template<typename T>
+  int operator*(T&& v) {
+    return x * v.y - y * v.x;
+  }
+
+  template<typename T>
+  int dot(T&& v) {
+    return (x * v.x + y * v.y);
+  }
+
+  template<typename T>
+  vec operator-(T&& v) {
+    return Vec(x - v.x, y - v.y);
+  }
+};
 
 int main(void) {
-cin.tie(0);
+  cin.tie(0);
   ios_base::sync_with_stdio(0);
 
-  cin >> N;
+  cin >> T;
 
-  int cnt = 0;
+  while (T--) {
+    int xs, ys, xe, ye, xl, yt, xr, yb;
+    cin >> xs >> ys >> xe >> ye >> xl >> yt >> xr >> yb;
 
-  deque<int> dq;
-  int sum = 2;
-  dq.push_back(2);
+    int cnt = 0;
+    if (xs >= xl && xs <= xr && ys <= yt && ys >= yb) cnt++;
+    if (xe >= xl && xe <= xr && ye <= yt && ye >= yb) cnt++;
 
-  while (sum > N) {
-    sum -= dq.front();
-    dq.pop_front();
-  }
-
-  if (sum == N) {
-    cnt++;
-  }
-
-  for (int i = 3; i <=N; i+=2) {
-    if (!isSosu(i)) continue;
-    dq.push_back(i);
-    sum += i;
-
-    while (sum > N) {
-      sum -= dq.front();
-      dq.pop_front();
+    if (cnt > 0) {
+      cout << "Y" << '\n';
+      continue;
     }
 
-    if (sum == N) {
-      cnt++;
+    vec vs(xs,ys);
+    vec ve(xe,ye);
+
+    vec va[4];
+    va[0] = vec(xl,yt);
+    va[1] = vec(xr,yt);
+    va[2] = vec(xl,yb);
+    va[3] = vec(xr,yb);
+
+    for (int i = 0; i < 4; i++) {
+      int i2 = (i + 1) % 4;
+
+      int cross1 = (ve - vs) * (va[i] - vs);
+      int cross2 = (ve - vs) * (va[i2] - vs);
+      int cross3 = (va[i2] - va[i]) * (vs - va[i]);
+      int cross4 = (va[i2] - va[i]) * (ve - va[i]);
+
+      if (cross1 * cross2 < 0 && cross3 * cross4 < 0) {
+        cout << "Y" << '\n';
+        break;
+      }
+      else if (cross1* cross2 == 0)
+
+
     }
   }
-
-  cout << cnt  << '\n';
   return 0;
 }
