d675b1cdca4a1bbd49d4e78cfa67684e5718cd0f
gyujin_choi <gyujin_choi@tmax.co.kr>
2023-03-06 18:50:28 +0900

    commit

--- cpp/acm/workspace.cpp
+++ cpp/acm/workspace.cpp
@@ -2,38 +2,53 @@
 #include <algorithm>
 
 using namespace std;
-using P1=pair<int,int>;
 
-int N, K;
-int arr[100000];
-int dif[100000];
+int N, T;
 
-bool cmp(P1& a, P1& b) {
-  return a.first<b.first;
+int arr[500];
+int dp[500][500];
+int sum[500];
+
+void calc(int l, int r) {
+  if (dp[l][r]>0) return;
+
+  if (l == r) dp[l][r] = 0;
+  else {
+    for (int i = l; i <r ; i++) {
+      calc(l,i);
+      calc(i+1,r);
+      int tmp = sum[r] - sum[l] + arr[l];
+      tmp += (dp[l][i]+dp[i+1][r]);
+      dp[l][r] = dp[l][r] == 0 ? tmp : min(dp[l][r],tmp);
+    }
+  }
+}
+
+
+void sol(){
+  cin >> N;
+
+  for(int i=0;i<N;i++) {
+    cin>>arr[i];
+
+    sum[i] = i==0? arr[i] : sum[i-1] + arr[i];
+  }
+
+  fill(&dp[0][0],&dp[499][500],0);
+
+  calc(0, N-1);
+  cout << dp[0][N-1] << '\n';
 }
 
 int main(void) {
   cin.tie(0);
   ios_base::sync_with_stdio(0);
 
-  cin >> N >> K;
-  int ans=0;
-  for (int i = 0; i < N; i++) {
-    cin >> arr[i];
-    // cout << arr[i] << '\n';
-    if (i!=0) {
-      dif[i-1] = arr[i]-arr[i-1];
-      ans += dif[i-1];
-    }
+  cin >> T;
+
+  while(T--){
+    sol();
   }
-
-  sort(dif,dif + N-1,greater<int>());
-
-  for (int i = 0; i < K-1; i++)
-    ans-=(dif[i]-1);
-
-  cout << ans +1<< '\n';
-
 
   return 0;
 }
