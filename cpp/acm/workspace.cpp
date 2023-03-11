cf21644d033829cd1f16387809d08e3d70e1d312
gyujin_choi <ckjin95@gmail.com>
2023-03-11 14:56:25 +0900

    cm

--- cpp/acm/workspace.cpp
+++ cpp/acm/workspace.cpp
@@ -1,51 +1,43 @@
 #include <iostream>
-#include <cmath>
+#include <queue>
 
 using namespace std;
 
-typedef pair <double,double> P1;
-
 int N;
-
-P1 arr[1000];
-
-int idx = -1;
-double MX = 10000000 * 4;
-
-static inline double dst(P1& a, P1& b) {
-  return sqrt(pow(b.first - a.first, 2) + pow(b.second - a.second, 2));
-}
+priority_queue<int> pq;
 
 int main(void) {
 
   cin.tie(0);
   ios_base::sync_with_stdio(0);
 
-
   cin >> N;
+
   for (int i = 0; i < N; i++) {
-    cin >> arr[i].first >> arr[i].second;
+    int tmp;
+    cin >> tmp;
+    pq.push(-tmp);
   }
 
-  int global_far_idx = 0;
-  for (int i = 0; i < N; i++) {
-    double far_dist = 0;
-    for (int j = 0; j < N; j++) {
-      if (i == j) continue;
-      double cur_dist = dst(arr[i], arr[j]);
-
-      far_dist = max(far_dist, cur_dist);
+  int cnt = 0;
+  long long ans = 0;
+  while (!pq.empty()) {
+    int tp = -pq.top();
+    tp -= cnt;
+    if (tp <= 0) {
+      pq.pop();
+      continue;
     }
-
-    if (MX > far_dist) {
-      MX = far_dist;
-      global_far_idx = i;
+    else {
+      cnt += 1;
+      tp -= 1;
+      if (tp > 0)
+        ans += tp;
+      pq.pop();
     }
   }
 
-  cout.precision(12);
-  cout << arr[global_far_idx].first << ' ' << arr[global_far_idx].second << '\n';
-  // cout << global_far_dist << '\n';
+  cout << ans << '\n';
 
   return 0;
 }
