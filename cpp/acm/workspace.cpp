690d81b04b8e5f9c42c8fbc143b71395ec5df91f
gyujin_choi <ckjin95@gmail.com>
2023-03-24 00:55:35 +0900

    cm

--- cpp/acm/workspace.cpp
+++ cpp/acm/workspace.cpp
@@ -1,35 +1,37 @@
 #include <iostream>
-#include <map>
 #include <algorithm>
+#include <deque>
+#include <vector>
 
 using namespace std;
 
-int G,P;
-int root[100001];
-
-int find_root(int i) {
-  if (root[i] == i) {
-    root[i] -= 1;
-    return root[i];
-  }
-  return root[i] = find_root(root[i]);
-}
+int N;
+vector<deque<int>> dv;
 
 int main(void) {
   cin.tie(0);
   ios_base::sync_with_stdio(0);
 
-  cin >> G >> P;
-  for (int i = 0; i <= G; i++) root[i] = i;
+  cin >> N;
+  for (int i = 1; i <= N; i++) {
 
-  int i;
-  for (i = 0; i < P; i++) {
-    int tmp; cin >> tmp;
-    int rt = find_root(tmp);
-    if (rt == -1) break;
+    int tmp;
+    cin >> tmp;
+
+    if (dv.size()==0) {
+      deque<int> dq;
+      dq.emplace_back(tmp);
+    }
+    else {
+      // 1. find
+      bool found = false;
+      for (auto& dq : dv) {
+        if (tmp < dq.front()) {
+          found = true;
+
+        }
+      }
+    }
   }
-
-  cout << i << '\n';
-
   return 0;
 }
